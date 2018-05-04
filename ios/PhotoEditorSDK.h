e47303bc5ea0dd4984f49124288f0598f6c12406
JoschaP <jp@php-pixel.de>
2018-05-04 16:02:31 +0200

    Changed Import of PhotoEditorSDK-Swift.h filge
    PhotoEditorSDK/PhotoEditorSDK-swift.h => PhotoEditorSDK/PhotoEditorSDK-Swift.h

--- ios/PhotoEditorSDK.h
+++ ios/PhotoEditorSDK.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "React/RCTBridgeModule.h"
-#import <PhotoEditorSDK/PhotoEditorSDK-swift.h>
+#import <PhotoEditorSDK/PhotoEditorSDK-Swift.h>
 
 @interface PhotoEditorSDK : NSObject <RCTBridgeModule, PESDKPhotoEditViewControllerDelegate>
 
